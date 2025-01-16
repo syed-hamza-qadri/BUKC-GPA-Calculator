be19b5ca2a97087a3cba739af1782ed17ad64f6f
Syed Hamza Qadri <qadrihamza871@gmail.com>
2025-01-16 11:54:38 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -124,6 +124,7 @@ const GPACalculator = () => {
                           <SelectValue placeholder="Select credit hours" />
                         </SelectTrigger>
                         <SelectContent>
+                          <SelectItem value="0">0</SelectItem>
                           <SelectItem value="1">1</SelectItem>
                           <SelectItem value="2">2</SelectItem>
                           <SelectItem value="3">3</SelectItem>
